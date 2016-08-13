b7592a0ea2b4088a03410ed800de0d613c5463d6
Kurimoto <hkurimoto@aiming-inc.com>
2016-08-13 14:43:51 +0900

    スコアによって表裏が入れ替わるようにした

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -9,9 +9,15 @@ public class Card : MonoBehaviour, IPointerDownHandler
     [SerializeField]
     Player Player;
     [SerializeField]
+    SpriteRenderer Front;
+    [SerializeField]
+    SpriteRenderer Back;
+
+    [SerializeField]
     int Place;
 
     public BigInteger ThisScore { get { return new BigInteger(1) << Place; } }
+    public bool IsOn { get { return (Player.Score >> Place & 1) > 0; } }
 
     // Use this for initialization
     void Start () {
@@ -20,8 +26,10 @@ public class Card : MonoBehaviour, IPointerDownHandler
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        var isOn = IsOn;
+        Front.gameObject.SetActive(isOn);
+        Back.gameObject.SetActive(!isOn);
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {

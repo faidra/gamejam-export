3391e1eb581159e04f1edfb608ea9a855408290e
Kurimoto <hkurimoto@aiming-inc.com>
2016-08-13 14:25:35 +0900

    カードがあり、タップでスコアが増える

---
+++ Assets/Card.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Card : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    Player Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Player.Score += 1;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using ScottGarland;
+
+public class Player : MonoBehaviour
+{
+    string ScoreString { get { return Score.ToString(); } }
+    public BigInteger Score = new BigInteger();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.logger.Log(ScoreString);
+    }
+}
